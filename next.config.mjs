b4684c5978358216e64854b0442202090f413f8c
Mohammed Habeebullah Khan <habeebkhan0075@gmail.com>
2026-04-25 11:37:36 +0530

    netlify.toml files has beed added.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'utf-8-validate': 'utf-8-validate',
+        'bufferutil': 'bufferutil',
+        'supports-color': 'supports-color',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
